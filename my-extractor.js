10568a65809713a77884f34d289f30bceef14f0f
Andrew Di Genova <drewdig77@gmail.com>
2017-05-22 21:38:00 -0400

    Update my-extractor.js

--- my-extractor.js
+++ my-extractor.js
@@ -8,7 +8,7 @@ var request = require("request"),
 request(url, function (error, response, body) {
   if (!error) {
     var $ = cheerio.load(body),
-      headline = $("h1").text(); 
+      headline = $(".article-title").text(); 
     
     console.log("My extracted text is -- " + headline);
   } else {
@@ -19,5 +19,7 @@ request(url, function (error, response, body) {
 // selectors
 // $("h1").text() returns Ionică Bizău The Joy of Being a Mentor How I npm What to Do When Your Website is Broken
 // $('h1:contains(Joy)') returns <h1>The Joy of Being a Mentor</h1>
-// $("#article-title") returns nothing
-// $("article-info").parent() returns nothing
+// $(".article-title").text() returns The Joy of Being a Mentor How I npm What to Do When Your Website is Broken
+// $(".article-info").parent() returns huge amount of text
+// $(".article-title > h1") returns <h1>The Joy of Being a Mentor</h1><h1>How I npm</h1><h1>What to Do When Your Website is Broken</h1>
+
